78e7066ac4def5f5110491f1ef0c2673bed0da96
nandhu24 <43338294+nandhu24@users.noreply.github.com>
2018-09-18 15:38:11 +0530

    Create file2.c

---
+++ file2.c
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("%d");
+scanf("%d",&a);
+if(a==0)
+{
+printf("zero");
+}
+else if(a>0)
+{
+printf("positive");
+}
+else if(a<0)
+{
+printf("negitive");
+}
+getch();
+}
